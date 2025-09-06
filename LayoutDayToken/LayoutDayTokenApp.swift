cd40df5942a840c2c408d604163443503a27a1fb
Adauto Oliveira <adauto.oliveira@me.com>
2025-09-06 18:27:32 -0300

    Initial Commit

---
+++ LayoutDayToken/LayoutDayTokenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutDayTokenApp.swift
+//  LayoutDayToken
+//
+//  Created by Adauto Oliveira on 06/09/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutDayTokenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
